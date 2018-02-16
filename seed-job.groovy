8b02e01461a3f9f5061ec4a4f4f97223d273ea9a
Erik Sommer <erotech@posteo.de>
2018-02-16 21:15:14 +0100

    seed job

---
+++ seed-job.groovy
@@ -0,0 +1,15 @@
+job('seed-job') {
+    logRotator(-1, 10)
+    scm {
+        github('the-it/jenkins_jobs', 'master')
+    }
+    triggers {
+        githubPush()
+    }
+    steps {
+        dsl {
+            external('*.groovy')
+            removeAction('DISABLE')
+        }
+    }
+}
